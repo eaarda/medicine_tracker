bef6ad4032de77424171f5e1de54fc79b70c7334
eaarda <elifaarda@gmail.com>
2021-03-01 21:29:09 +0300

    submitted medicine table into db

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Medicine(models.Model):
+    medicine_name = models.CharField(max_length=80)
+    dosage = models.IntegerField()
+    frequency = models.IntegerField()
