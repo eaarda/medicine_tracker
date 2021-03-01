b52bb4010e97ec054b8d594e57fe4beadc0f83bd
eaarda <elifaarda@gmail.com>
2021-03-01 23:28:25 +0300

    migrated

--- tracker/models.py
+++ tracker/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Medicine(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     medicine_name = models.CharField(max_length=80)
     dosage = models.IntegerField()
     frequency = models.CharField(max_length=80)
